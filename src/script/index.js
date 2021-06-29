b2d7968630b01aa84eeefa67ce5d6787268d7b13
Fanbayy <fandi.bayu110@gmail.com>
2021-06-29 17:27:26 +0700

    go go go

---
+++ src/script/index.js
@@ -0,0 +1,9 @@
+var myVar;
+function myFunction() {
+  myVar = setTimeout(showPage, 1000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
